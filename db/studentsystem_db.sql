5127b66810dd73f744523597510377c75ec74f8b
cyeryllekayeveraces <cyeryllekayeveraces@gmail.com>
2026-04-20 23:52:20 +0800

    Update studentsystem_db.sql

--- db/studentsystem_db.sql
+++ db/studentsystem_db.sql
@@ -1,47 +1,52 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 19, 2026 at 07:50 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+-- ----------------------------
+-- DATABASE
+-- ----------------------------
+CREATE DATABASE IF NOT EXISTS `studentsystem`;
+USE `studentsystem`;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `studentsystem`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `studentsystem_db`
---
-
+-- ----------------------------
+-- TABLE STRUCTURE
+-- ----------------------------
 CREATE TABLE `studentsystem_db` (
-  `student_id` varchar(50) NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `gender` varchar(30) NOT NULL,
-  `birthdate` date NOT NULL,
-  `address` varchar(100) NOT NULL,
-  `contact_number` varchar(50) NOT NULL,
-  `course_id` varchar(50) NOT NULL,
-  `course_name` varchar(50) NOT NULL,
-  `course_code` varchar(50) NOT NULL,
-  `enrollment_id` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-COMMIT;
+  `student_id` VARCHAR(50) NOT NULL,
+  `first_name` VARCHAR(50) NOT NULL,
+  `last_name` VARCHAR(50) NOT NULL,
+  `gender` VARCHAR(30) NOT NULL,
+  `birthdate` DATE NOT NULL,
+  `address` VARCHAR(100) NOT NULL,
+  `contact_number` VARCHAR(50) NOT NULL,
+  `course_id` VARCHAR(50) NOT NULL,
+  `course_name` VARCHAR(100) NOT NULL,
+  `course_code` VARCHAR(50) NOT NULL,
+  `enrollment_id` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- ----------------------------
+-- SAMPLE DATA (FOR TESTING)
+-- ----------------------------
+INSERT INTO `studentsystem_db` (
+  `student_id`,
+  `first_name`,
+  `last_name`,
+  `gender`,
+  `birthdate`,
+  `address`,
+  `contact_number`,
+  `course_id`,
+  `course_name`,
+  `course_code`,
+  `enrollment_id`
+) VALUES
+('2026-001', 'Juan', 'Dela Cruz', 'Male', '2005-01-01', 'Cagayan', '09123456789', 'BSIT-01', 'BS Information Technology', 'BSIT', 'ENR-001'),
+('2026-002', 'Maria', 'Santos', 'Female', '2004-05-12', 'Tuguegarao', '09987654321', 'BSCS-01', 'Computer Science', 'BSCS', 'ENR-002');
+
+COMMIT;
